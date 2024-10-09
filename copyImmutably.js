324a4d7ae74352272a9b4f4601d3c600bef41c62
jubaerahmedtamim <jubaerahmedtamim2014@gmail.com>
2024-10-10 02:57:32 +0600

    reference vs primitive

---
+++ copyImmutably.js
@@ -0,0 +1,10 @@
+const language = {
+    name: 'JavaScript',
+    estd: 1995,
+}
+const language2 = {...language}
+
+language.founder = "Brendan Eich";
+
+console.log(language);
+console.log(language2);
---
+++ referenceType.js
@@ -0,0 +1,11 @@
+let a = { name: 'Sakib', age: 32};
+let b = a;
+
+a.name = "Tamim"; //mutate/ change
+
+
+a = {}; //assignment
+
+console.log(a);
+console.log(b);
+
